75463744a616ea58571b7155de10ee042afbab05
zorg9pi9 <zorg99900000@mail.ru>
2018-05-06 18:02:38 +0200

    Changes

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,11 +1,5 @@
 import org.junit.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
 
 public class BookTest {
